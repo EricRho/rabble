6ac4133c2794355239bd001091482ab12ed3cc6e
Eric Rho <ericrho@EricMacbookPro.fios-router.home>
2017-03-29 13:48:05 -0400

    Add remaining main pages

--- js/angular/route_controller.js
+++ js/angular/route_controller.js
@@ -1,21 +1,4 @@
-// // 'use strict';
-//
-// app.config(['$routeProvider', function($routeProvider) {
-//   $routeProvider
-//   .when('/', {
-//     // templateUrl: 'partials/home.html'
-//     templateUrl: 'partials/about.html',
-//     controller: 'aboutCtrl'
-//   })
-//   .when('/#about', {
-//     // templateUrl: 'partials/about.html',
-//     templateUrl: 'partials/home.html',
-//     controller: 'homeCtrl'
-//   })
-//   .otherwise({
-//     redirectTo: '/'
-//   });
-// }]);
+'use strict';
 
 app.config(['$routeProvider', function($routeProvider) {
   $routeProvider
@@ -26,5 +9,25 @@ app.config(['$routeProvider', function($routeProvider) {
     .when('/about', {
       templateUrl: 'partials/about.html',
       controller: 'aboutCtrl'
+    })
+    .when('/activies', {
+      templateUrl: 'partials/activies.html',
+      controller: 'activitiesCtrl'
+    })
+    .when('/gallery', {
+      templateUrl: 'partials/gallery.html',
+      controller: 'galleryCtrl'
+    })
+    .when('/availability', {
+      templateUrl: 'partials/availability',
+      controller: 'availabilityCtrl'
+    })
+    .when('/contact', {
+      templateUrl: 'partials/contact.html',
+      controller: 'contactCtrl'
+    })
+    .when('/faq', {
+      templateUrl: 'partials/faq.html',
+      controller: 'faqCtrl'
     })
 }]);
