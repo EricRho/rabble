6cf7751473553743462bdbf1c9c4fe98655e714f
Eric Rho <ericrho@EricMacbookPro.fios-router.home>
2017-03-29 12:11:32 -0400

    Commit changes. Add git ignore

--- js/angular/aboutCtrl.js
+++ js/angular/aboutCtrl.js
@@ -1,4 +1,39 @@
 app.controller('aboutCtrl', ['$scope', function($scope) {
     $scope.message = 'About Controller Working'
     console.log('About Controller');
+
+
+
+    $(document).ready(function() {
+      console.log('JQuery test');
+
+      var $cont = document.querySelector('.cont');
+
+      var $elsArr = [].slice.call(document.querySelectorAll('.el'));
+      var $closeBtnsArr = [].slice.call(document.querySelectorAll('.el__close-btn'));
+
+      setTimeout(function() {
+        $cont.classList.remove('s--inactive');
+      }, 200);
+      $elsArr.forEach(function($el) {
+        $el.addEventListener('click', function() {
+          console.log($el);
+          if (this.classList.contains('s--active')) return;
+          $cont.classList.add('s--el-active');
+          this.classList.add('s--active');
+        });
+      });
+
+      $closeBtnsArr.forEach(function($btn) {
+        $btn.addEventListener('click', function(e) {
+          e.stopPropagation();
+          $cont.classList.remove('s--el-active');
+          document.querySelector('.el.s--active').classList.remove('s--active');
+        });
+      });
+
+
+    }); // JQUERY
+
+
 }]);
--- js/angular/homeCtrl.js
+++ js/angular/homeCtrl.js
@@ -1,5 +1,106 @@
 app.controller('homeCtrl', ['$scope', function($scope) {
 
-    $scope.message = 'Home Controller Working'
+    $scope.message = 'Home Controller Working';
     console.log('Home Controller');
+
+    $(document).ready(function() {
+		var ticking = false,
+			isFirefox = (/Firefox/i.test(navigator.userAgent)),
+			isIe = (/MSIE/i.test(navigator.userAgent)) || (/Trident.*rv\:11\./i.test(navigator.userAgent)),
+			scrollSensitivitySetting = 30,
+			slideDurationSetting = 600,
+			currentSlideNumber = 0,
+			totalSlideNumber = $(".background").length;
+
+		// ------------- DETERMINE DELTA/SCROLL DIRECTION ------------- //
+		function parallaxScroll(evt) {
+	  		if (isFirefox) {
+		    	// DELTA FOR FIREFOX
+		    	delta = evt.deltaY * (-120);
+					console.log('FIREFOX DELTA', delta);
+
+		  	} else if (isIe) {
+		    	// DELTA FOR IE
+		    	delta = -evt.deltaY;
+		  	} else {
+		    	// DELTA FOR OTHER BROWSERS
+		    	delta = evt.wheelDelta;
+		    	console.log('DELTA', delta);
+		  	}
+
+		  	debugger;
+
+		  	if (ticking != true) {
+		    	if (delta <= -scrollSensitivitySetting) {
+			      	// Down scroll
+			      	ticking = true;
+		      		// if (currentSlideNumber !== totalSlideNumber - 1) {
+	      			if (currentSlideNumber < totalSlideNumber - 1) {
+		      			console.log('all good');
+		        		currentSlideNumber ++;
+		        		console.log('CURRENT SLIDE', currentSlideNumber);
+		        		nextItem();
+		      		}
+		      		slideDurationTimeout(slideDurationSetting);
+		    	}
+		    	if (delta >= scrollSensitivitySetting) {
+		      		// Up scroll
+		      		ticking = true;
+		      		if (currentSlideNumber !== 0) {
+		        		currentSlideNumber --;
+		      		}
+		      		previousItem();
+		      		slideDurationTimeout(slideDurationSetting);
+		    	}
+		  	}
+		}
+
+		// ------------- SET TIMEOUT TO TEMPORARILY "LOCK" SLIDES ------------- //
+		function slideDurationTimeout(slideDuration) {
+		  	setTimeout(function() {
+		    	ticking = false;
+		  	}, slideDuration);
+		}
+
+		// ------------- ADD EVENT LISTENER ------------- //
+		var mousewheelEvent = isFirefox ? "DOMMouseScroll" : "wheel";
+		window.addEventListener('wheel', throttle(parallaxScroll, 60), false);
+		// window.addEventListener(mousewheelEvent, _.throttle(parallaxScroll, 60), false);
+
+		// ------------- SLIDE MOTION ------------- //
+		function nextItem() {
+		  	var $previousSlide = $(".background").eq(currentSlideNumber - 1);
+		  	$previousSlide.removeClass("up-scroll").addClass("down-scroll");
+		}
+
+		function previousItem() {
+			var $currentSlide = $(".background").eq(currentSlideNumber);
+			$currentSlide.removeClass("down-scroll").addClass("up-scroll");
+		}
+
+		function throttle(fn, threshold, scope) {
+			console.log('throttle');
+			console.log('THRESHOLD BEFORE', threshold);
+			threshold || (threshold = 60);
+			console.log('THRESHOLD', threshold);
+			var last,
+				deferTimer;
+			return function () {
+				var context = scope || this;
+				var now = +new Date,
+					args = arguments;
+				if (last && now < last + threshold) {
+					clearTimeout(deferTimer);
+					deferTimer = setTimeout(function() {
+						last = now;
+						fn.apply(context, args);
+					}, threshold);
+				} else {
+					last = now;
+					fn.apply(context, args);
+				}
+			};
+		}
+});
+
 }]);
